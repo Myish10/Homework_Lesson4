9ea753db2e6777f46def9ad0e55ccc5dc665c6c3
adya <myish1084@gmail.com>
2023-06-20 21:30:45 +0300

    Task 3

---
+++ Task3/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+// int InputNum(string message)
+// {
+//     Console.WriteLine(message);
+//     return int.Parse(Console.ReadLine()!);
+// }
+
+int[] ArrayRand(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(0, 101);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    int size = array.Length;
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write(array[i]+" ");
+    }
+}
+
+// int len = InputNum("Введите длину массива: ");
+int[] arr = ArrayRand(8);
+PrintArray(arr);
