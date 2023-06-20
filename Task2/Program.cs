fde51fc3ef31c12a47238e1e80dca9b9aa796894
adya <myish1084@gmail.com>
2023-06-20 21:01:45 +0300

    Task 2

---
+++ Task2/Program.cs
@@ -0,0 +1,24 @@
+﻿//  Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int InputNum(string message)
+{
+    Console.WriteLine(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+int SumNum(int num)
+{
+    int sum = 0;
+    while (num != 0)
+    {
+        sum = sum + num % 10;
+        num /= 10;
+    }
+    return sum;
+}
+int number = InputNum("Введите целое число: ");
+int sumNumber = SumNum(number);
+Console.WriteLine(sumNumber);
